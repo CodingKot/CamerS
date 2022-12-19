2b610f243a3ae2df2217ab05bfb02e374f68927d
CodingKot <90643541+CodingKot@users.noreply.github.com>
2022-12-19 22:28:11 +0300

    second iteration corrections

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,10 +1,6 @@
 import {createAction} from '@reduxjs/toolkit';
 import { AppRoute } from '../const';
-import { Cameras } from '../types/camera';
 
 export const redirectToRoute = createAction<AppRoute | string>('redirectToRoute');
-export const pressUp = createAction<Cameras>('list/pressUp');
-export const pressDown = createAction<Cameras>('list/pressDown');
-export const selectItem = createAction<number>('list/selectItem');
 export const resetSearchedCameras = createAction('cameras/resetSearchedCameras');
 
--- src/types/sorting-params.ts
+++
@@ -1,4 +0,0 @@
-export type SortingParams = {
-  sortType: string;
-  order: string;
-}
